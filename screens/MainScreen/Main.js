d49c4a7a49404d69d271aecd585f102dd72fb0d9
hyderali0889 <hyderali0889@gmail.com>
2022-02-17 14:49:39 +0500

    main

---
+++ screens/MainScreen/Main.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { MainImproved } from "./MainStyles";
+
+export const Main = () => {
+  return (
+    <MainImproved color="blue" >
+      <Text> Main Screen </Text>
+    </MainImproved>
+  );
+};
---
+++ screens/MainScreen/MainStyles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const MainImproved = styled.View`
+backgroundColor :${(props) => props.color};
+
+`
